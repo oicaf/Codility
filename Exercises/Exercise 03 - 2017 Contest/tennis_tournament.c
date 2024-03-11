7d3d9cba1652aa118fcc1eef22199e06538202e9
oicaf <162804996+oicaf@users.noreply.github.com>
2024-03-11 18:37:57 +0100

    Create tennis_tournament.c

---
+++ Exercises/Exercise 03 - 2017 Contest/tennis_tournament.c
@@ -0,0 +1,14 @@
+/*
+Task: https://app.codility.com/programmers/trainings/3/tennis_tournament/
+Score: https://app.codility.com/demo/results/training9WHEUM-UKW/
+*/
+
+int solution(int P, int C)
+{
+    int pairs = P / 2, games;
+
+    if(pairs <= C)
+        return games = pairs;
+    else
+        return games = C;
+}
