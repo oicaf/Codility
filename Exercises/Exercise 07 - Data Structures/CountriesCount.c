c0e7163e9c5de4fefba93ec0173fc6d46eb8d75d
oicaf <162804996+oicaf@users.noreply.github.com>
2026-01-18 09:06:17 +0100

    Add files via upload

---
+++ Exercises/Exercise 07 - Data Structures/CountriesCount.c
@@ -0,0 +1,106 @@
+/*
+Task: https://app.codility.com/programmers/trainings/7/countries_count/
+Score: https://app.codility.com/demo/results/trainingM29PEW-PJH/
+
+Algorithm:
+Traversing the entire array recursively using the DFS method. For each array element, checks whether it has
+already been visited:
+- if not, then all neighbors that meet the appropriate condition are visited using the DFS method, marked as
+'visited', and after the visit is completed, the 'countries' counter is incremented by 1 (the given country
+is "painted"),
+- if so, then it means that it belongs to a country "painted" in the previous process, and the next array
+element is taken under the consideration and etc.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void dfs(int **A, bool **visited, int r, int c, int N, int M)
+{
+    int row = r, col = c;
+
+    // moving right
+    if ((col + 1 <= M - 1) && A[row][col + 1] == A[row][col] && visited[row][col + 1] == 0)
+    {
+        col++;
+        visited[row][col] = 1;
+        dfs(A, visited, row, col, N, M);
+        col--;
+    }
+    // moving down
+    if ((row + 1 <= N - 1) && A[row + 1][col] == A[row][col] && visited[row + 1][col] == 0)
+    {
+        row++;
+        visited[row][col] = 1;
+        dfs(A, visited, row, col, N, M);
+        row--;
+    }
+    // moving left
+    if ((col - 1) >= 0 && A[row][col - 1] == A[row][col] && visited[row][col - 1] == 0)
+    {
+        col--;
+        visited[row][col] = 1;
+        dfs(A, visited, row, col, N, M);
+        col++;
+    }
+    // moving up
+    if ((row - 1) >= 0 && A[row - 1][col] == A[row][col] && visited[row - 1][col] == 0)
+    {
+        row--;
+        visited[row][col] = 1;
+        dfs(A, visited, row, col, N, M);
+        row++;
+    }
+
+    return;
+}
+
+int solution(int **A, int N, int M)
+{
+    int i, j, countries = 0;
+
+    bool **visited = malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++)
+        visited[i] = malloc(M * sizeof(int));
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < M; j++)
+            visited[i][j] = 0;
+
+    for(i = 0; i < N; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            if (visited[i][j] == 0)
+            {
+                visited[i][j] = 1;
+                dfs(A, visited, i, j, N, M);
+                countries++;
+            }
+        }
+    }
+
+    return countries;
+}
+
+int main(void)
+{   
+    int N = 7, M = 3;
+
+    int **A = malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++)
+        A[i] = malloc(M * sizeof(int));
+
+    A[0][0] = 5;    A[0][1] = 4;    A[0][2] = 4;
+    A[1][0] = 4;    A[1][1] = 3;    A[1][2] = 4;
+    A[2][0] = 3;    A[2][1] = 2;    A[2][2] = 4;
+    A[3][0] = 2;    A[3][1] = 2;    A[3][2] = 2;
+    A[4][0] = 3;    A[4][1] = 3;    A[4][2] = 4;
+    A[5][0] = 1;    A[5][1] = 4;    A[5][2] = 4;
+    A[6][0] = 4;    A[6][1] = 1;    A[6][2] = 1;
+
+    printf("%d\n", solution(A, N, M));
+
+    return 0;
+}
