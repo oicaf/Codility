a6f15a3998725e2dc86a75c3a9da6e95e7d58552
oicaf <162804996+oicaf@users.noreply.github.com>
2026-02-09 15:04:10 +0100

    Update SlalomSkiing.c

--- Exercises/Exercise 01 - 2015 Contest/SlalomSkiing.c
+++ Exercises/Exercise 01 - 2015 Contest/SlalomSkiing.c
@@ -1,6 +1,6 @@
 /*
 Task: https://app.codility.com/programmers/trainings/1/slalom_skiing/
-Score: https://app.codility.com/demo/results/training4PRU7Q-SWD/
+Score: https://app.codility.com/demo/results/trainingSTU3TM-ES9/
 
 Strategy:
 The LIS (Longest Increasing Subsequence) method was used here in a modified version for the task.
@@ -12,12 +12,14 @@ The LIS (Longest Increasing Subsequence) method was used here in a modified vers
 int lower_bound(long long int *tails, int size, long long int x) {
     int left = 0, right = size, mid;
 
-    while (left < right) {
-        mid = left + (right - left) / 2;
-        if (tails[mid] >= x)
-            right = mid;
-        else
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (x < tails[mid])
+            right = mid - 1;
+        else if (x > tails[mid])
             left = mid + 1;
+        else
+            right = mid;
     }
     return left;
 }
@@ -25,7 +27,10 @@ int lower_bound(long long int *tails, int size, long long int x) {
 int solution(int A[], int N) {
     int i, j = 0, max = 0, len = 0, pos;
     long long int *T = malloc((3 * N) * sizeof(long long int));
-    long long int *tails = malloc((3 * N) * sizeof(long long int));
+    long long int *tails = malloc((3 * N + 1) * sizeof(long long int));
+
+    for (i = 0; i < 3 * N + 1; i++)
+        tails[i] = -1;
 
     for (i = 0; i < N; i++)
         max = A[i] > max ? A[i] : max;
@@ -37,12 +42,16 @@ int solution(int A[], int N) {
         T[j++] = A[i];
     }
 
-for (i = 0; i < 3 * N; i++) {
-    pos = lower_bound(tails, len, T[i]);
-    tails[pos] = T[i];
-    if (pos == len)
-        len++;
-}
+    tails[0] = -1;
+    for (i = 0; i < 3 * N; i++) {
+        pos = lower_bound(tails, len, T[i]);
+        if (tails[pos] == -1) {
+            tails[pos] = T[i];
+            len++;
+        }
+        else
+            tails[pos] = (tails[pos] < T[i]) ? tails[pos] : T[i];
+    }
 
     free(T);
     free(tails);
